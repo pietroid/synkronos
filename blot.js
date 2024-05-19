d00d193b69c508e3d1e27cd8e624343d3fe4a7d9
pietroid <^pietrodomingues@gmail.com^>
2024-05-19 20:07:59 -0300

    Now working

--- blot.js
+++ blot.js
@@ -1,7 +1,19 @@
 class Blot {
-    constructor(x, y, size) {
+    constructor(x, y, size, factor1) {
         this.x = x;
         this.y = y;
         this.size = size;
+        this.factor1 = factor1;
+        this.availableMass = 1;
     }
+
+    isTouching(other) {
+        let d = dist(this.x, this.y, other.x, other.y);
+        return d < this.size / 2 + other.size / 2;
+    }
+
+}
+
+dist = function(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
--- sketch.js
+++ sketch.js
@@ -1,44 +1,44 @@
 var blots = [];
 var total_energy = 0;
+var energy_rate =0;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(100);
-    
+	background(0);
 }
 
 function draw() {
-    let energy_rate = 1;
+    background(0);
     //How blots will evolve?
-    total_energy += energy_rate
-    
-    // generating new blots
-    let virtual_energy_level = Math.random() * 10;
-    if(total_energy > virtual_energy_level){
-        let x = Math.random() * width;
-        let y = Math.random() * height;
-        let factor = 2;
-        size = factor * virtual_energy_level;
-        blots.push(new Blot(x, y, size));
-        total_energy -= virtual_energy_level;
-    }
+    total_energy += 2;
 
-    // evolving existing blots
-    for (let i = 0; i < blots.length; i++) {
-        let blot = blots[i];
-        
-        // degradation rate
-        blot.size -= 0.1;
-        
-        if(blot.size <= 0){
-            delete blots[i];
-        }else{
-            blot.size += 1;
+    let creationState = Math.random() > 0.3;
+
+    if(creationState){
+        let x = random(width);
+        let y = random(height);
+        let size = random(10, 50);
+        let factor1 = random(0.1, 2);
+        let blot = new Blot(x, y, size, factor1);
+        blots.push(blot);
+    }else{
+        for(var i = 0; i < blots.length; i++){
+            let blot = blots[i];
+            blot.size -= blot.factor1 * 2;
+            blot.size += blot.factor1 * blot.availableMass;
+            blot.availableMass -= 0.05;
+            blot.size -= 0.1 * blot.size;
+            if(blot.size < 0){
+                blots.splice(i, 1);
+            }
         }
     }
 
+    
     // drawing blots
     for (let blot of blots) {
-        ellipse(blot.x, blot.y, blot.size, blot.size);
+        if(blot != undefined){
+            ellipse(blot.x, blot.y, blot.size, blot.size);
+        }
     }
 }
